9d304b78632a9aff89272ceefabe6d06da637448
gavinator9 <epikdude@rocketmail.com>
2023-05-10 21:36:29 -0700

    Add files via upload
    ultimate banking application

---
+++ multiuserbank_elliott/banking/Program.cs
@@ -0,0 +1,171 @@
+﻿// Gavin Elliott
+// IT112
+// NOTES: this was a fun assignment, thought I had it completely done in one hour then spent more time combing over the fine details to fit criteria than I expected
+// BEHAVIORS NOT IMPLIMENTED AND WHY: seems to be good as far as I can see
+
+namespace banking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Banking World! Enter your login credentials. \n");
+            string input = "";
+            bool inuse = true;
+            bank account = new bank(10000);
+            bool empty = false;
+            char loggedin= '0';
+            empty= false;
+            decimal balancereader = 0;
+            while (inuse==true)
+            {
+
+                while (loggedin=='0')
+                {
+                    Console.WriteLine("The bank currently contains " + account.bankbalance.ToString("C") + " worth of money.");
+                    string user;
+                    string pass;
+                    Console.WriteLine("Please enter your login information \n");
+                    Console.WriteLine("Username?:\n");
+                    user=Console.ReadLine();
+                    Console.WriteLine("Password?:\n");
+                    pass=Console.ReadLine();
+                    if (user== "jlennon")
+                    {
+                        if (pass == "johnny")
+                        {
+                            loggedin = '1';
+                            Console.WriteLine("Welcome John Lennon!\n");
+                            balancereader = account.balancejohn;
+                        }
+                    }
+
+                    if (user == "pmccartney")
+                    {
+                        if (pass == "pauly")
+                        {
+                            loggedin = '2';
+                            Console.WriteLine("Welcome Paul Mccartney!\n");
+                            balancereader = account.balancepaul;
+                        }
+                    }
+
+                    if (user == "gharrison")
+                    {
+                        if (pass == "georgy")
+                        {
+                            loggedin = '3';
+                            Console.WriteLine("Welcome George Harrison!\n");
+                            balancereader = account.balancegeorge;
+                        }
+                    }
+
+                    if (user == "rstarr")
+                    {
+                        if (pass == "ringoy")
+                        {
+                            loggedin = '4';
+                            Console.WriteLine("Welcome Ringo Starr!\n");
+                            balancereader = account.balanceringo;
+                        }
+                    }
+
+                }
+
+                if (balancereader==0 || account.bankbalance==0) 
+                {
+                    Console.WriteLine("Your cannot withdraw while the bank is empty silly guy");
+                        empty=true;
+                }
+                if (balancereader>0&&account.bankbalance>0)
+                {
+                    empty=false;
+                }
+
+                if (empty == true)
+                {
+                    if (loggedin == '4')
+                    {
+                        Console.WriteLine("rstarr bank account\n Current balance is: "+account.balanceringo.ToString("C"));
+                    }
+                    if (loggedin == '3')
+                    {
+                        Console.WriteLine("gharrison bank account\n Current balance is: "+account.balancegeorge.ToString("C"));
+                    }
+                    if (loggedin == '2')
+                    {
+                        Console.WriteLine("pmccartney bank account\n Current balance is: " + account.balancepaul.ToString("C"));
+                    }
+                    if (loggedin == '1')
+                    {
+                        Console.WriteLine("jlennon bank account\n Current balance is: " + account.balancejohn.ToString("C"));
+                    }
+                    Console.WriteLine("\nEnter a to deposit, c to quit or d to logout. \n");
+                }
+                if (empty==false)
+                {
+                    if (loggedin == '4')
+                    {
+                        Console.WriteLine("rstarr bank account\n Current balance is: " + account.balanceringo.ToString("C"));
+                    }
+                    if (loggedin == '3')
+                    {
+                        Console.WriteLine("gharrison bank account\n Current balance is: " + account.balancegeorge.ToString("C"));
+                    }
+                    if (loggedin == '2')
+                    {
+                        Console.WriteLine("pmccartney bank account\n Current balance is: " + account.balancepaul.ToString("C"));
+                    }
+                    if (loggedin == '1')
+                    {
+                        Console.WriteLine("jlennon bank account\n Current balance is: " + account.balancejohn.ToString("C"));
+                    }
+
+
+                    Console.WriteLine("\nEnter a to deposit, b to withdraw, c to quit, or d to logout. \n");
+                }
+                input = Console.ReadLine();
+                if (input =="a")
+                {
+                    Console.WriteLine("Enter your deposit amount. \n");
+                    input=Console.ReadLine();
+                    decimal deposit=decimal.Parse(input);
+                    string output = account.deposit(deposit,loggedin);
+                    Console.WriteLine(output);
+                    balancereader = account.balanceindex;
+
+                }
+                if (input=="b" && empty==false) 
+                {
+
+                    Console.WriteLine("Enter Withdrawal amount. \n");
+                    input=Console.ReadLine();
+                    decimal withdrawal=decimal.Parse(input);
+                    if (withdrawal > 500) 
+                    {
+                        withdrawal = 500;
+                        Console.WriteLine("500 is the maximum withdrawal, reducing withdrawal to 500. \n");
+                    }
+                    string output=account.withdraw(withdrawal,loggedin);
+                    Console.WriteLine(output);
+                    balancereader = account.balanceindex;
+                }
+
+
+                if (input=="c")
+                {
+                    inuse = false;
+                    Console.WriteLine("\nGoodbye!");
+
+                }
+
+                if (input=="d")
+                {
+                    loggedin = '0';
+                }
+
+
+            }
+        }
+    }
+}
---
+++ multiuserbank_elliott/banking/bank.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banking
+{
+    internal class bank
+    {
+
+
+        private decimal _balancejohn=1250;
+        private decimal _balancepaul=2500;
+        private decimal _balanceringo=1000;
+        private decimal _balancegeorge=3000;
+        private decimal _bankbalance= 10000;
+        private decimal _balanceindex = 0;
+
+
+        public decimal balanceindex
+        {
+            get { return _balanceindex; }
+            set { _balanceindex = value; }
+        }
+
+
+        public decimal bankbalance
+        {
+            get { return _bankbalance; }
+            set { _bankbalance = value; }
+        }
+
+        public decimal balancejohn
+        {
+            get { return _balancejohn; }
+            set { _balancejohn = value; }
+        }
+
+        public decimal balancepaul
+        {
+            get { return _balancepaul; }
+            set { _balancepaul = value; }
+        }
+
+        public decimal balanceringo
+        {
+            get { return _balanceringo; }
+            set { _balanceringo = value; }
+        }
+
+        public decimal balancegeorge
+        {
+            get { return _balancegeorge; }
+            set { _balancegeorge = value; }
+        }
+
+        public bank(decimal balance)
+        {
+            _bankbalance= balance;
+        }
+
+        public string deposit(decimal input, char account)
+        {
+            string output = "";
+            if (account == '1')
+            {
+                _balancejohn += input;
+                _balanceindex = _balancejohn;
+                _bankbalance += input;
+                output = "Your new higher balance is: " + _balancejohn.ToString("C");
+
+            }
+            if (account == '2')
+            {
+                _balancepaul += input;
+                _balanceindex = _balancepaul;
+                _bankbalance += input;
+                output = "Your new higher balance is: " + _balancepaul.ToString("C");
+            }
+            if (account == '3')
+            {
+                _balancegeorge += input;
+                _balanceindex = _balancegeorge;
+                _bankbalance+= input;
+                output = "Your new higher balance is: " + _balancegeorge.ToString("C");
+            }
+            if (account== '4')
+            {
+                _balanceringo += input;
+                _balanceindex = _balanceringo;
+                _bankbalance += input;
+                output = "Your new higher balance is: " + _balanceringo.ToString("C");
+            }
+            return output;
+        
+        
+        }
+
+        public string withdraw(decimal input, char account)
+        {
+            string output="da bank is empty";
+
+            if (_bankbalance>0)
+            {
+                if (account == '1')
+                {
+                    if (input > _balancejohn)
+                    {
+                        _bankbalance -= _balancejohn;
+                    }
+                    else
+                    {
+                        _bankbalance -= input;
+                    }
+                    _balancejohn -= input;
+                    _balanceindex = _balancejohn;
+
+
+                    if (_balancejohn < 0)
+                    {
+                        _balancejohn = 0;
+                        _balanceindex = _balancejohn;
+                    }
+                    output = "Your new withdrawn balance is: " + _balancejohn.ToString("C");
+
+                }
+
+                if (account == '2')
+                {
+                    if (input > _balancepaul)
+                    {
+                        _bankbalance -= _balancepaul;
+                    }
+                    else
+                    {
+                        _bankbalance -= input;
+                    }
+                    _balancepaul -= input;
+                    _balanceindex = _balancepaul;
+
+                    if (_balancepaul < 0)
+                    {
+                        _balancepaul = 0;
+                        _balanceindex = _balancepaul;
+                    }
+                    output = "Your new withdrawn balance is: " + _balancepaul.ToString("C");
+
+                }
+
+                if (account == '3')
+                {
+                    if (input > _balancegeorge)
+                    {
+                        _bankbalance -= _balancegeorge;
+                    }
+                    else
+                    {
+                        _bankbalance -= input;
+                    }
+                    _balancegeorge -= input;
+                    _balanceindex = _balancegeorge;
+
+                    if (_balancegeorge < 0)
+                    {
+                        _balancegeorge = 0;
+                        _balanceindex = _balancegeorge;
+                    }
+                    output = "Your new withdrawn balance is: " + _balancegeorge.ToString("C");
+
+                }
+
+                if (account == '4')
+                {
+                    if (input > _balanceringo)
+                    {
+                        _bankbalance -= _balanceringo;
+                    }
+                    else
+                    {
+                        _bankbalance -= input;
+                    }
+                    _balanceringo -= input;
+                    _balanceindex = _balanceringo;
+
+                    if (_balanceringo < 0)
+                    {
+                        _balanceringo = 0;
+                        _balanceindex = _balanceringo;
+                    }
+                    output = "Your new withdrawn balance is: " + _balanceringo.ToString("C");
+
+                }
+            }
+
+            if (_bankbalance<0)
+            {
+                _bankbalance = 0;
+            }
+
+            return output;
+        }
+
+
+
+
+    }
+}
